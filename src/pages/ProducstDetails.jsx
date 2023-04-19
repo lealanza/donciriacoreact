a5e8c9bca227ab62f189b5e7082db83dcef43aa2
Leandro Lanza <tucholanza@gmail.com>
2023-04-18 22:23:28 -0300

    detalles productos

--- src/pages/ProducstDetails.jsx
+++ src/pages/ProducstDetails.jsx
@@ -1,8 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import { useParams } from 'react-router-dom'
+import products from '../data/data'
+import Helmet from '../components/Helmet/Helmet'
+import CommonSection from '../components/Ui/CommonSection'
+import '../styles/product-details.css'
+import { motion } from 'framer-motion'
 
 const ProducstDetails = () => {
+  const [tab, setTab] = useState('desc')
+  const { id } = useParams()
+  const product = products.find((item) => item.id.toString() === id)
+  const { imgUrl, productName, price, avgRating, reviews, description, shortDesc, exp, category } = product;
   return (
-    <div>ProducstDetails</div>
+    <Helmet title={productName}>
+      <CommonSection title={productName} />
+      <section className='pt-0'>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <img src={imgUrl} alt={id} />
+            </Col>
+            <Col lg='6'>
+              <div className="product__details">
+                <h2>{productName}</h2>
+                <div className="product__rating d-flex align-items-center mb-4">
+                  <span><i class="ri-star-fill"></i>S</span>
+                  <span><i class="ri-star-fill"></i></span>
+                  <span><i class="ri-star-fill"></i></span>
+                  <span><i class="ri-star-fill"></i></span>
+                  <span><i class="ri-star-half-line"></i></span>
+                  <span>
+                    <span>({avgRating}</span>Rating)
+                  </span>
+                </div>
+                <div>
+                  <p className='product__price'>${price}</p>
+                  <span className='mt-4'>{shortDesc}</span>
+                </div>
+                <motion.button whileTap={{ scale: 1.05 }} className='buy__btn text-white'>Comprar</motion.button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+                        <Container>
+                          <Row>
+                            <Col lg='12'>
+                              {
+                                (category === 'mtb' || category === 'gravel' || category === 'ruta') && (
+                                  <div className='product__exp'>
+                                    <h6 className='exp__title'>Experiencia</h6>
+                                    <ul>
+                                      <li>Horquilla: {exp?.horquilla}</li>
+                                      <li>Cuadro: {exp?.cuadro}</li>
+                                    </ul>
+                                  </div>
+                                )
+                              }
+                            </Col>
+                          </Row>
+                        </Container>
+                      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              <div className='tab__wrapper d-flex align-item-center gap-5 mt-5'>
+                <h6 className={`${tab === 'desc' ? 'active__tab' : ''}`} onClick={() => setTab('desc')}>Descripcion</h6>
+                <h6 className={`${tab === 'rev' ? 'active__tab' : ''}`} onClick={() => setTab('rev')}>Reviews ({reviews.length})</h6>
+
+
+              </div>
+
+              <div className="tab__content">
+                <p className='mt-4'>{description}</p>
+              </div>
+
+              <div className='product__review mt-4'>
+                <div className="review__wrapper">
+                  <ul>
+                    {
+                      reviews?.map((item, index) => (
+                        <li key={index}>
+                          <span>Clasificación: {item.rating}</span>
+                          <p>{item.text}</p>
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </div>
+              </div>
+
+
+
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+
+    </Helmet>
   )
 }
 
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Helmet from '../components/Helmet/Helmet'
+import CommonSection from '../components/Ui/CommonSection'
+import { Container, Row, Col} from 'reactstrap'
+import {FcSearch} from 'react-icons/fc'
+import '../styles/shop.css'
+import products from '../data/data'
+import ProductsList from'../components/Ui/ProductsList'
 
 const Shop = () => {
+  const [productsData, setProductsData]= useState(products)
+  const handleFilter = (e) => {
+    const filterValue = e.target.value;
+    const filteredProducts =
+      filterValue === "todos"
+        ? products
+        : products.filter((item) => item.category === filterValue);
+
+    setProductsData(filteredProducts);
+  };
+
+  const handleSearch = (e) => {
+    const searchTern = e.target.value
+    const searchedProducts = products.filter(item => item.productName.toLocaleLowerCase().includes(searchTern.toLocaleLowerCase()))
+    setProductsData(searchedProducts)
+  }
   return (
     <Helmet title={"Shop"}>
-       <div>Shop</div>
+       <CommonSection title="Productos"/>
+       <section>
+        <Container>
+          <Row>
+            <Col lg='3' md='3'>
+              <div className="filter__widget">
+                <select onChange={handleFilter}>
+                  <option value='todos'>Filtro por Categorias</option>
+                  <option value="accesorios">Accesorios</option>
+                  <option value="cascos">Cascos</option>
+                  <option value="componentes">Componentes</option>
+                  <option value="mtb">Mountain Bikes</option>
+                  <option value="gravel">Gravel</option>
+                  <option value="ruta">Ruta</option>
+                  <option value="repuestos">Repuestos</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='3' md='4'>
+            <div className="filter__widget">
+                <select>
+                  <option>Ordenar por</option>
+                  <option value="ascendente">Ascendente</option>
+                  <option value="descendente">Descendente</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='6' md='6'>
+              <div className="search__box">
+                <input  type="text" placeholder='Buscar......' onChange={handleSearch}/> <FcSearch className='search__icon'/>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+       </section>
+       <section className='pt-0'>
+        <Container>
+          <Row>
+            {
+              productsData.length===0 ? (
+              <h1 className='text-center fs-4'>No hay productos!!</h1>
+              ):
+              <ProductsList data={productsData}/>
+            }
+          </Row>
+        </Container>
+       </section>
     </Helmet>
    
   )
